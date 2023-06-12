0679e3ed41a640c0beb75b43b0f6be55ef48be09
William Ahern <willdahern@gmail.com>
2023-06-11 21:50:42 -0400

    feat: add personal ec2 stack for provisiong an ec2, instance role, and security group in the default vpc

--- lib/app.ts
+++ lib/app.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { PipelineStack } from './codepipeline-test-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { PipelineStack } from "./codepipeline-test-stack";
+import { PersonalEc2Stack } from "./personal-ec2-stack";
 
 const app = new cdk.App();
-new PipelineStack(app, 'PipelineStack', {});
-
+new PipelineStack(app, "PipelineStack", {});
+new PersonalEc2Stack(app, "PersonalEc2Stack", {});
 app.synth();
---
+++ lib/personal-ec2-stack.ts
@@ -0,0 +1,65 @@
+import { App, CfnOutput, Stack, StackProps } from "aws-cdk-lib";
+import {
+  Instance,
+  InstanceClass,
+  InstanceSize,
+  InstanceType,
+  MachineImage,
+  Peer,
+  Port,
+  SecurityGroup,
+  Vpc,
+} from "aws-cdk-lib/aws-ec2";
+import { Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
+
+export class PersonalEc2Stack extends Stack {
+  constructor(scope: App, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const defaultVpc = Vpc.fromLookup(this, "DefaultVpc", {
+      isDefault: true,
+    });
+
+    const instanceRole = new Role(this, "InstanceRole", {
+      roleName: "InstanceRole",
+      assumedBy: new ServicePrincipal("ec2.amazonaws.com"),
+    });
+
+    const instanceSecurityGroup = new SecurityGroup(
+      this,
+      "InstanceSecurityGroup",
+      {
+        vpc: defaultVpc,
+        allowAllOutbound: true,
+        securityGroupName: "InstanceSecurityGroup",
+      }
+    );
+    instanceSecurityGroup.addIngressRule(
+      Peer.anyIpv4(),
+      Port.tcp(22),
+      "Allow SSH access from the internet."
+    );
+    instanceSecurityGroup.addIngressRule(
+      Peer.anyIpv4(),
+      Port.tcp(80),
+      "Allow HTTP access from the internet."
+    );
+    instanceSecurityGroup.addIngressRule(
+      Peer.anyIpv4(),
+      Port.tcp(443),
+      "Allow HTTPS access from the internet."
+    );
+
+    const instance = new Instance(this, "Instance", {
+      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.NANO),
+      vpc: defaultVpc,
+      securityGroup: instanceSecurityGroup,
+      role: instanceRole,
+      machineImage: MachineImage.latestAmazonLinux2(),
+    });
+
+    new CfnOutput(this, "InstancePublicIp", {
+      value: instance.instancePublicIp,
+    });
+  }
+}
